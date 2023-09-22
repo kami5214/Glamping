02e00f2b34729bb2a505cf03f1071ac7653f4486
Darcko03 <138030432+Darcko03@users.noreply.github.com>
2023-09-21 20:12:57 -0500

    Update SeederTablaPermisos.php

--- database/seeders/SeederTablaPermisos.php
+++ database/seeders/SeederTablaPermisos.php
@@ -29,7 +29,29 @@ class SeederTablaPermisos extends Seeder
          'ver-cliente',
          'crear-cliente',
          'editar-cliente',
-         'borrar-cliente'         
+         'borrar-cliente',
+            //tabla-caracteristicas
+            'ver-caracteristica',
+            'crear-caracteristica',
+            'editar-caracteristica',
+            'borrar-caracteristica',
+            //tabla-domos
+            'ver-domo',
+            'crear-domo',
+            'editar-domo',
+            'borrar-domo',
+                  
+                        //tabla-servicios
+                        'ver-servicio',
+                        'crear-servicio',
+                        'editar-servicio',
+                        'borrar-servicio', 
+                                   //tabla-usuarios
+                                   'ver-usuario',
+                                   'crear-usuario',
+                                   'editar-usuario',
+                                   'borrar-usuario'    
+                                               
         ];
 foreach($permisos as $permiso){
     Permission::create([ 'name'=>$permiso]);
