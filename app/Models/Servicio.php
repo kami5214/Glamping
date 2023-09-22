9f29fad1aab49dc333323b460f2ae35b675fb658
Cami <133257394+kami5214@users.noreply.github.com>
2023-09-22 18:09:18 -0500

    Update Servicio.php

--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class servicio extends Model
+class Servicio extends Model
 {
     use HasFactory;
     protected $fillable = ['ser_nombre', 'ser_estado', 'ser_precio','ser_cantidad'
